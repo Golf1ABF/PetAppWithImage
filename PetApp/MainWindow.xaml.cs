3c1b69e4bd81f26d40ba9215db6f14b303688537
Avx <93741404+Golf1ABF@users.noreply.github.com>
2024-05-21 09:30:19 +0200

    Add project files.

---
+++ PetApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetApp
+{
+    public partial class MainWindow : Window
+    {
+        List<Pet> pets;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pets = new List<Pet>();
+            var sr = new StreamReader("../../../src/animals.txt", System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) 
+            {
+                pets.Add(new Pet(sr.ReadLine()));
+            }
+            foreach (var item in pets)
+            {
+                lbx.Items.Add($"{item.Name}, Kor: {item.Age}, Szín: {item.Color}");    
+            }
+            lbx.Items.SortDescriptions.Add(
+            new SortDescription("", ListSortDirection.Ascending));
+            lbx.SelectedItem = lbx.Items.GetItemAt(0);
+        }
+
+        private void Fav_Click(object sender, RoutedEventArgs e)
+        {
+            if (!lbxFav.Items.Contains(lbx.SelectedItem))
+            {
+                lbxFav.Items.Add(lbx.SelectedItem);
+            }
+            else MessageBox.Show("Már a kedvencek között van ez az állat!", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (lbxFav.SelectedItem != null)
+            {
+                lbxFav.Items.Remove(lbxFav.SelectedItem);
+            }
+            else MessageBox.Show("Nincs mit törölni!", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btnImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbx.SelectedItem != null)
+            {
+                var selectedPetInfo = lbx.SelectedItem.ToString().Split(',')[0].Trim();
+                var selectedPet = pets.Find(pet => pet.Name == selectedPetInfo);
+
+                if (selectedPet != null)
+                {
+                    string currentDirectory = Directory.GetCurrentDirectory();
+                    imgPet.Source = new BitmapImage(new Uri(System.IO.Path.Combine(currentDirectory, $@"..\..\..\IMAGES\{selectedPet.Picture}")));
+                }
+            }
+        }
+    }
+}
---
+++ PetApp/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public string Picture { get; set; }
+
+        public Pet(string line)
+        {
+            string[] data = line.Split(";");
+            Name = data[0];
+            Age = int.Parse(data[1]);
+            Color = data[2];
+            Picture = data[3];
+        }
+    }
+}
